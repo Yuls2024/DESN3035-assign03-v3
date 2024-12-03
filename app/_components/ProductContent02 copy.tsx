5322dcd21f02fcfcbde19cef170139f794775522
Yuls2024 <yulichka00@hotmail.com>
2024-12-03 16:34:13 +0000

    Fixing styling

---
+++ app/_components/ProductContent02 copy.tsx
@@ -0,0 +1,28 @@
+export default function ProductContent02() {
+    return (
+        <>
+            <main>
+                <div className="productItemContainer">
+                    <div className="itemContainer">
+                        <div className="itemText">
+                            <section id="itemEntry01" className="itemEntry">
+                                <div><img className="itemPhoto" src="images/flowers02.jpg" alt="Hat Box Arrangement photo" />
+                                </div>
+                                <div>
+                                    <h2>Hat Box Arrangement</h2>
+                                    <h3>$75 CAD</h3>
+                                    <p>Bright and cheerful, a hat box arrangement featuring a sunflower and vibrant blooms bursts with color and joy, perfect for uplifting any space.</p>
+
+                                    <p>This stunning arrangement blends the bold beauty of a sunflower with an array of other colorful flowers, creating a lively and eye-catching display.</p>
+
+                                    <p>Presented in an elegant hat box, this unique floral design brings warmth, happiness, and a touch of sophistication to any celebration or setting.</p>
+                                </div>
+                            </section>
+                            <a className="goBack" href="./">Back to Catalog</a>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </>
+    );
+}
